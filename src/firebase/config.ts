18505d97c5d9cb2a692cc80da2abd496b2f76c58
Sourav Rout <sourav.9911rout@gmail.com>
2026-01-06 07:56:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9087915921-2b2f3",
+  "appId": "1:1026518137752:web:9b9ef862e38ea8915c49ed",
+  "apiKey": "AIzaSyArrxgUYVH2xruHysObvabCI53gbqhL0xg",
+  "authDomain": "studio-9087915921-2b2f3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1026518137752"
+};
